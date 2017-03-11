3fab4e75b5c2295b40addf14205574a8e2d8fb3a
Kiyan <fourcels@gmail.com>
2017-03-11 16:51:02 +0800

    测试域名

--- app/config.js
+++ app/config.js
@@ -5,7 +5,12 @@
 // 此处主机域名是腾讯云解决方案分配的域名
 // 小程序后台服务解决方案：https://www.qcloud.com/solution/la
 
-var host = "apitest.ipaotui.com"
+// var host = "apitest.ipaotui.com"
+var host = "api.ipaotui.com"
+const debug = wx.getStorageSync('debug')
+if(debug) {
+    host = "apitest.ipaotui.com"
+}
 
 var config = {
 
